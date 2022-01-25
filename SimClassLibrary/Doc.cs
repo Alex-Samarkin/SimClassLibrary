c8f4c868f78b6ec9c4abe9fa197ba3297752e5bb
alex_ <alexsamarkinru@gmail.com>
2022-01-25 13:58:54 +0300

    first commit

---
+++ SimClassLibrary/Doc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimClassLibrary
+{
+    public static class Doc
+    {
+        public static TopicList Intro { get; set; } = new TopicList();
+        public static TopicList MainList { get; set; } = new TopicList();
+        public static TopicList EndList { get; set; } = new TopicList();
+
+        public static void MakeIntro()
+        {
+            Intro.Theme = "Введение";
+            Intro.Add(new Topic()
+                {
+                    Title = "Цель", 
+                    Content = "Создание небольшой библиотеки для симуляции процессов", 
+                    Author = "Жаров, Самаркин", 
+                    Comment = "", 
+                    DT = DateTime.Today
+                }
+                );
+        }
+
+
+    }
+}
---
+++ SimClassLibrary/Topic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimClassLibrary
+{
+    public class Topic
+    {
+        public string Title { get; set; } = "Заголовок раздела";
+        public string Content { get; set; } = "Содержание раздела";
+        public string Comment { get; set; } = "Комментарий";
+        public string Author { get; set; } = "Автор";
+        public DateTime DT { get; set; } = DateTime.Today;
+
+        public void Setup(string title, string content, string comment, string author, DateTime dt)
+        {
+            Title = title;
+            Content = content;
+            Comment = comment;
+            Author = author;
+            DT = dt;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Title)}: {Title}, {nameof(Content)}: {Content}, {nameof(Comment)}: {Comment}, {nameof(Author)}: {Author}, {nameof(DT)}: {DT}";
+        }
+        public string ToPrettyString()
+        {
+            string hr = new string('=', 120)+"\n";
+            string hr1 = "\n"+new string('-', 120)+ "\n";
+            return $"{hr}" +
+                   $"{nameof(Title)}: {Title}" +hr1+
+                   $"{nameof(Content)}: {Content}" +hr1+
+                   $"{nameof(Comment)}: {Comment}" +hr1+
+                   $"{nameof(Author)}: {Author}" +hr1+
+                   $"{nameof(DT)}: {DT}"+"\n"+hr;
+        }
+
+    }
+
+    public class TopicList
+    {
+        public string Theme { get; set; } = "Theme";
+        public List<Topic> Topics { get; set; } = new List<Topic>();
+
+        public void Add(Topic t)
+        {
+            Topics.Append(t);
+        }
+    }
+}
